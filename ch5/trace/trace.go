684cd144a73145b9e80f4d3efaf9ceccbed13cb6
Charles Chong <ilovesoup@fastmail.com>
2022-07-29 14:47:22 -0500

    ch5

--- ch5/trace/trace.go
+++ ch5/trace/trace.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"time"
 )
 
 func bigSlowOperation() {
 	defer trace("bigSlowOperation")()
 	// ... lots of work...
-	time.Sleep(10 * time.Second)
+	time.Sleep(1 * time.Second)
 }
 
 func trace(msg string) func() {
@@ -17,6 +19,50 @@ func trace(msg string) func() {
 	return func() { log.Printf("exit %s (%s)", msg, time.Since(start)) }
 }
 
+// func double(x int) int {
+// 	return x + x
+// }
+func double(x int) (result int) {
+	defer func() { fmt.Printf("double(%d) = %d\n", x, result) }()
+	return x + x
+}
+
+func triple(x int) (result int) {
+	defer func() { result += x }()
+	return double(x)
+}
+
+func readFiles(filenames []string) error {
+	for _, filename := range filenames {
+		f, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		defer f.Close() // NOTE: risky; could run out of file descriptors
+	}
+	return nil
+}
+
+func readFiles2(filenames []string) error {
+	for _, filename := range filenames {
+		if err := doFile(filename); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func doFile(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	// ...
+	return nil
+}
+
 func main() {
 	bigSlowOperation()
+
+	_ = double(4)
 }
