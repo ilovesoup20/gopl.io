799614f331a03d524e539fa146cc52ed574810ba
Charles Chong <ilovesoup@fastmail.com>
2022-07-30 16:47:31 -0500

    ch5

--- ch6/geometry/geometry.go
+++ ch6/geometry/geometry.go
@@ -5,18 +5,20 @@ import (
 	"math"
 )
 
+// Point ...
 type Point struct{ X, Y float64 }
 
-// traditional function
+// Distance ... traditional function
 func Distance(p, q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
-// same thing, but as a method of the Point type
+// Distance ... same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
+// Path ...
 type Path []Point
 
 // Distance returns the distance traveled along the path
