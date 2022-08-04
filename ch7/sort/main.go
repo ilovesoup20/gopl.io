30445055d3efbc9242f0593fc40136aa8c941e5f
Charles Chong <ilovesoup@fastmail.com>
2022-08-04 14:18:15 -0500

    ch7

---
+++ ch7/sort/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type StringSlice []string
+
+func (p StringSlice) Len() int           { return len(p) }
+func (p StringSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p StringSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+func main() {
+	strings := StringSlice{"f", "a", "w", "i", "v"}
+
+	fmt.Println(strings)
+	sort.Sort(strings)
+	fmt.Println(strings)
+}
